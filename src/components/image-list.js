9f7c23227e0822022cc93c1a1985ce99586aa5cc
benjamin-summers <summerstyler2@gmail.com>
2018-11-30 23:52:39 -0500

    fixing some minor issue with images

--- src/components/image-list.js
+++ src/components/image-list.js
@@ -1,12 +1,10 @@
 import './ImageList.css';
 import React from 'react';
+import ImageCard from './imageCard';
 
 const ImageList = (props) => {
-    console.log(props.images);
     const imageList = props.images.map((image) => {
-        return (
-            <img key={image.id} src={image.urls.regular} alt={image.description}/>
-        )
+        return <ImageCard key={image.id} image={image} />
     })
 
     return <div className="image-list">{imageList}</div>;
